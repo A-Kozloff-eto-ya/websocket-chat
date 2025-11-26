ebafc480c6502f49b18b71c2bcff5d272ddd7d00
solnyshko_eto_ya <true.solnyshko@gmail.com>
2025-11-26 21:17:56 +0300

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,19 +2,8 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'], // ← это уже подключит Tailwind
   nitro: {
-    prerender: {
-      crawlLinks: false,
-      routes: ['/sitemap.xml']
-    },
     experimental: {
       websocket: true
-    }
-  },
-  $production: {
-    nitro: {
-      prerender: {
-        crawlLinks: false
-      }
     }
   }
 })
