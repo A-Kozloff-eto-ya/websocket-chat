4e1f99f6fd2ada3a53aa871bb1538debacc6734c
solnyshko_eto_ya <true.solnyshko@gmail.com>
2025-11-26 20:56:44 +0300

    init

--- app/plugins/socket.client.ts
+++ app/plugins/socket.client.ts
@@ -2,7 +2,7 @@
 import { io, type Socket } from "socket.io-client";
 
 export default defineNuxtPlugin(() => {
-  let socket: Socket = io("ws://localhost:3000", { autoConnect: false }); // выключаем автоподключение
+  let socket: Socket = io("ws://websocket-chat-blush.vercel.app", { autoConnect: false }); // выключаем автоподключение
 
   return {
     provide: {
